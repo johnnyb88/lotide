6043a21b6436a8bd1974d8b1831595323a848229
johnnyb88 <johnnybarratt@gmail.com>
2019-09-16 22:56:23 +0000

    Function that return first item in array

--- head.js
+++ head.js
@@ -6,6 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(arr) {
+  return arr[0];
+}
+
 // TEST CODE
-assertEqual("Lighthouse LabsBootcamp", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

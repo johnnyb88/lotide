29eee7571388a049bd84ca4279a837db7cd3528f
johnnyb88 <johnnybarratt@gmail.com>
2019-09-17 21:40:33 +0000

    lotide function comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🌈 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛑ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+     if( arr1[i]!== arr2[i]) {
+       return false;
+     };
+  }
+  return true
+}
+
+
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
+assertEqual(eqArrays([2, 5, 3, 6], [3, 4, 1]), true);

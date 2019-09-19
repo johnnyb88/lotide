f3f28a38f5664dc464e5df3ca1270f6b8ecd12e9
johnnyb88 <johnnybarratt@gmail.com>
2019-09-19 21:47:52 +0000

    takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertEqualArrays = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("The arrays match!");
+  } else {
+    console.log("The arrays do not match!");
+  }
+};
+
+// function that returns "slice of the array with
+// elements taken from the beggining." it should
+// keep going until the callback/predicate returns truthy value
+//the callback should only be provided the item in the array
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let item of array) {
+    if (callback(item) !== true) {
+      result.push(item);
+    } else {
+      return result;
+    }
+  }
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);

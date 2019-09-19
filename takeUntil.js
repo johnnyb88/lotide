a67581f699997666526a4035363d7e3792bfeb40
johnnyb88 <johnnybarratt@gmail.com>
2019-09-19 22:18:21 +0000

    new created findkey function

--- takeUntil.js
+++ takeUntil.js
@@ -28,8 +28,9 @@ const takeUntil = function(array, callback) {
       return result;
     }
   }
-}
+};
 
+//TEST CODE
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);

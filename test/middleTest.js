6450b73cdad03726eb83f4e6dc849c3cbc250ee3
johnnyb88 <johnnybarratt@gmail.com>
2019-09-23 21:34:09 +0000

    refractored tests for middle and tail files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,18 @@
 const middle = require("../middle");
-const assertEqualArrays = require("../assertArraysEqual");
+const assert = require("chai").assert;
 
+describe("middle", () => {
 
+  it("should return the middle element of an odd number length array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
 
-assertEqualArrays(middle([1, 2, 3, 4, 5]), [3]);
-assertEqualArrays(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertEqualArrays(middle([1, 2]), []);
-assertEqualArrays(middle([1]), []);
+  });
+
+  it("should return the 2 middle elements when given an even number array", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("should return an empty array when given 2 or less elements in an array",() => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,21 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
+
+
+describe("tail", () => {
+
+  it("should return ['Lighthouse, Labs'] when given array: ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]),["Lighthouse", "Labs"]);
+    
+  });
+});
 
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-assertEqual(tail(words).length, 3);
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+// assertEqual(tail(words).length, 3);

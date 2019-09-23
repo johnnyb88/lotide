f49dd90f57a362933ce7d857c0164fe86a75cad3
johnnyb88 <johnnybarratt@gmail.com>
2019-09-23 20:00:28 +0000

    Tests for modified lotide modules

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const tail = require("../tail");
+const assertEqual = require("../assertEqual");
+
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+assertEqual(tail(words).length, 3);
